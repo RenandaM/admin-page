57d0ac819ef7111e5102bd56bddcba204242c757
RenandaM <renanda.m.eriyan@gmail.com>
2024-07-05 16:07:13 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+const Orders = [
+    {
+        productName: 'Black Tea',
+        productNumber: '63487',
+        paymentStatus: 'Due',
+        status: 'Pending',
+    },
+    {
+        productName: 'Instant Pancake',
+        productNumber: '26355',
+        paymentStatus: 'Refunded',
+        status: 'Declined',
+    },
+    {
+        productName: 'Maple Sirup',
+        productNumber: '76234',
+        paymentStatus: 'Paid',
+        status: 'Active',
+    },
+]
---
+++ script.js
@@ -0,0 +1,66 @@
+// darkmode
+const sideMenu = document.querySelector('aside');
+const menuBtn = document.getElementById('menu-btn');
+const closeBtn = document.getElementById('close-btn');
+
+const darkMode = document.querySelector('.dark-mode');
+
+menuBtn.addEventListener('click', () => {
+    sideMenu.style.display = 'block';
+});
+
+closeBtn.addEventListener('click', () => {
+    sideMenu.style.display = 'none';
+});
+
+darkMode.addEventListener('click', () => {
+    document.body.classList.toggle('dark-mode-variables');
+    darkMode.querySelector('span:nth-child(1)').classList.toggle('active');
+    darkMode.querySelector('span:nth-child(2)').classList.toggle('active');
+});
+
+// chart
+var options = {
+    series: [{
+    name: 'series1',
+    data: [31, 40, 28, 51, 42, 109, 100]
+  }, {
+    name: 'series2',
+    data: [11, 32, 45, 32, 34, 52, 41]
+  }],
+    chart: {
+    height: 350,
+    type: 'area'
+  },
+  dataLabels: {
+    enabled: false
+  },
+  stroke: {
+    curve: 'smooth'
+  },
+  xaxis: {
+    type: 'datetime',
+    categories: ["2018-09-19T00:00:00.000Z", "2018-09-19T01:30:00.000Z", "2018-09-19T02:30:00.000Z", "2018-09-19T03:30:00.000Z", "2018-09-19T04:30:00.000Z", "2018-09-19T05:30:00.000Z", "2018-09-19T06:30:00.000Z"]
+  },
+  tooltip: {
+    x: {
+      format: 'dd/MM/yy HH:mm'
+    },
+  },
+  };
+
+  var chart = new ApexCharts(document.querySelector("#chart"), options);
+  chart.render();
+
+Orders.forEach(order =>{
+    const tr = document.createElement('tr');
+    const trContent =`
+        <td>${order.productName}</td>
+        <td>${order.productNumber}</td>
+        <td>${order.paymentStatus}</td>
+        <td class="${order.status === 'Declined' ? 'danger' : order.status === 'Pending' ? 'warning' : 'primary'}">${order.status}</td>
+        <td class="primary">Details</td>
+    `;
+    tr.innerHTML = trContent;
+    document.querySelector('table tbody').appendChild(tr);
+})
